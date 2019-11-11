a40b0090a68c1770c9ff0d890f2afc7118bffe1e
StubbornGras <g1090035743@gmail.com>
2019-11-11 12:12:12 +0800

    提交测试

---
+++ src/Test.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Test.php
+ * 描述
+ *
+ *
+ * @copyright Copyright (c) 2018-2019 http://www.gamerpark.com
+ * @link    http://www.gamerpark.com
+ * @author  StubbornGrass
+ * @email   g1090035743@gmail.com
+ * @license 游民公园项目组版权所有
+ * @version 1.0
+ *
+ */
+
+namespace StubbornGrass\UserFaker;
+class Test {
+    public function test() {
+
+        echo "HelloWorld";
+    }
+}
